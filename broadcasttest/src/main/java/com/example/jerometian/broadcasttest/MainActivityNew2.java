1f5df158940d3459422647044cc8bb5e18e628ce
jjtian <jerometian@gmail.com>
2015-10-26 17:01:41 +0800

    boradcast test

---
+++ broadcasttest/src/main/java/com/example/jerometian/broadcasttest/MainActivityNew2.java
@@ -0,0 +1,17 @@
+package com.example.jerometian.broadcasttest;
+
+import android.app.Activity;
+import android.content.IntentFilter;
+
+/**
+ * Created by jjtian on 2015/10/26.
+ */
+public class MainActivityNew2 extends Activity{
+    private IntentFilter intentFilter;
+    private LocalReceiver localReceiver;
+
+
+    private class LocalReceiver {
+
+    }
+}
