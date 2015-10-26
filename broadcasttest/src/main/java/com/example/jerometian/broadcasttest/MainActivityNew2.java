f5c55f3e0440ecd4a17d0f95636fe9c5a67882a9
jerometian <jerometian@gmail.com>
2015-10-26 21:29:06 +0800

    learning  force off line

--- broadcasttest/src/main/java/com/example/jerometian/broadcasttest/MainActivityNew2.java
+++ broadcasttest/src/main/java/com/example/jerometian/broadcasttest/MainActivityNew2.java
@@ -1,17 +1,56 @@
 package com.example.jerometian.broadcasttest;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by jjtian on 2015/10/26.
  */
 public class MainActivityNew2 extends Activity{
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        localBroadcastManager.unregisterReceiver(localReceiver);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        Button button = (Button)findViewById(R.id.Button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 Intent intent = new Intent("com.example.jerometian.broadcasttest.LOCAL_BROADCAST");
+                localBroadcastManager.sendBroadcast(intent);
+            }
+
+        });
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.example.jerometian.broadcasttest.LOCAL_BROADCAST");
+        localReceiver = new LocalReceiver();
+        localBroadcastManager.registerReceiver(localReceiver,intentFilter);
+    }
+
     private IntentFilter intentFilter;
     private LocalReceiver localReceiver;
+    private LocalBroadcastManager localBroadcastManager;
 
+     class LocalReceiver  extends BroadcastReceiver {
 
-    private class LocalReceiver {
-
-    }
+         @Override
+         public void onReceive(Context context, Intent intent) {
+             Toast.makeText(context,"Received local Broadcast.",Toast.LENGTH_LONG).show();
+         }
+     }
 }
