763f63a86157801679c26e1e9f52b33c3117b2bb
jjtian <jerometian@gmail.com>
2015-10-28 16:27:35 +0800

    learning broadcast test 1028 16:27

--- broadcastbestpractice/src/main/java/com/example/jerometian/broadcastbestpractice/LoginActivity.java
+++ broadcastbestpractice/src/main/java/com/example/jerometian/broadcastbestpractice/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.example.jerometian.broadcastbestpractice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.AppCompatButton;
+import android.support.v7.widget.AppCompatCheckBox;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -21,6 +24,12 @@ public class LoginActivity  extends BaseActivity{
     private AppCompatEditText accountEdit;
 
     private AppCompatButton login;
+
+    private AppCompatCheckBox rememberpassword;
+
+    private SharedPreferences pref;
+
+    private SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,8 +51,19 @@ public class LoginActivity  extends BaseActivity{
 
         passwordEdit = (AppCompatEditText)findViewById(R.id.password);
 
+        rememberpassword = (AppCompatCheckBox)findViewById(R.id.remember);
+        pref =  PreferenceManager.getDefaultSharedPreferences(this);
         login = (AppCompatButton)findViewById(R.id.login);
+        boolean isRemember = pref.getBoolean("remember_password",false);
+        if (isRemember)
+        {
+            String account = pref.getString("account", "");
+            String password = pref.getString("password","");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberpassword.setChecked(true);
 
+        }
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -51,6 +71,17 @@ public class LoginActivity  extends BaseActivity{
                 String password  = passwordEdit.getText().toString();
                 if ( account.equals("admin") && password.equals("123456"))
                 {
+                    editor = pref.edit();
+                    if ( rememberpassword.isChecked())
+                    {
+                        editor.putBoolean("remember_password",true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+                    }else
+                    {
+                        editor.clear();
+                    }
+                    editor.commit();
                     Intent intent = new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(intent);
                     finish();
