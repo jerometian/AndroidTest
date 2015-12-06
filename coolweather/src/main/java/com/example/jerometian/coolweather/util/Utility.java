e36f07621f9099400896c9521c8dbe0479b533d0
jerometian <jerometian@gmail.com>
2015-12-06 20:20:39 +0800

    add parse xml class

--- coolweather/src/main/java/com/example/jerometian/coolweather/util/Utility.java
+++ coolweather/src/main/java/com/example/jerometian/coolweather/util/Utility.java
@@ -3,6 +3,8 @@ package com.example.jerometian.coolweather.util;
 import android.text.TextUtils;
 
 import com.example.jerometian.coolweather.db.CoolWeatherDB;
+import com.example.jerometian.coolweather.model.City;
+import com.example.jerometian.coolweather.model.County;
 import com.example.jerometian.coolweather.model.Province;
 
 /**
@@ -30,5 +32,53 @@ public class Utility {
         }
         return  false;
 
+    }
+
+
+    public synchronized  static boolean handleCitiesResponse(CoolWeatherDB coolWeatherDB, String response,int provinceId)
+    {
+        if (!TextUtils.isEmpty(response))
+        {
+
+            String[] allCities = response.split(",");
+            if ( allCities !=null && allCities.length> 0)
+            {
+                for(String c:allCities)
+                {
+                    String[] array = c.split("\\|");
+                    City city  = new City();
+                    city.setCityCode(array[0]);
+                    city.setCityName(array[1]);
+                    city.setProvinceId(provinceId);
+                    // 将解析出来的数据存储到City表
+                    coolWeatherDB.saveCity(city);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public synchronized boolean handleCountiesResponse(CoolWeatherDB coolWeatherDB,String response,int cityId)
+    {
+        if ( !TextUtils.isEmpty(response))
+        {
+            String[] allCounties = response.split(",");
+            if ( allCounties != null && allCounties.length > 0)
+            {
+                for (String c: allCounties)
+                {
+                    String[] array = c.split("\\|");
+                    County county = new County();
+                    county.setCountyCode(array[0]);
+                    county.setCountyName(array[1]);
+                    county.setCityId(cityId);
+                    coolWeatherDB.saveCounty(county);
+                }
+                return true;
+            }
+
+        }
+        return false;
     }
 }
