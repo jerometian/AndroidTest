fd6c78299dd28affcb006f8c97c5a4148943be2f
jerometian <jerometian@gmail.com>
2015-12-06 11:13:37 +0800

    add db class

---
+++ coolweather/src/main/java/com/example/jerometian/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package com.example.jerometian.coolweather.model;
+
+/**
+ * Created by jerometian on 2015/12/5.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+}
